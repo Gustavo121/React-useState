47f91dedb3663e2564417537b71d3c0eb3f4ab32
Gustavo <gustgouvea@hotmail.com>
2022-08-22 12:09:25 -0300

    variavel/estado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [title, setTitle] = useState('Hello World 3');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>  
+      <h1>{title}</h1>
     </div>
   );
 }
